43da617986b745374df028cd304fb2606e6d91fd
fatima-mousavi224 <fatimamousavi224@gmail.com>
2026-02-09 09:05:02 +0430

    create next applaction

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-amber-300 font-black">
+        Wellcome to next.js 👋
+        </h1>
+    </div>
+  )
+}
+
+export default Home
