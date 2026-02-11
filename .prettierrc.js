ac92b5b7dd19fb5e7b00b3ae51049a63c90a18f7
fatima-mousavi224 <fatimamousavi224@gmail.com>
2026-02-11 09:44:23 +0430

    Set up Light & Dark Theme , Shadcn, Navbar

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true, // استفاده از ; در انتهای خطوط
+  singleQuote: true, // کوتیشن ' به جای "
+  printWidth: 100, // طول خط قبل از شکستن
+  tabWidth: 2, // فاصله تب
+  trailingComma: 'es5', // کامای آخر در آرایه و آبجکت
+  bracketSpacing: true, // فاصله بین آکولادها
+  arrowParens: 'always', // پرانتز برای آرگومان تک در arrow function
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-amber-300 font-black">
-        Wellcome to next.js 👋
-        </h1>
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-3xl text-amber-300 font-black dark:text-white">Wellcome to next.js 👋</h1>
+     
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
